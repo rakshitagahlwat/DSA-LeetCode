dca0d72522d7666d2fe6627aabebf55d3ac8f1d1
rakshitagahlwat <rakshitakumar2006@gmail.com>
2026-08-16 23:12:16 +0530

    Time: 7 ms (29.05%), Space: 114.8 MB (64.86%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        
+        // Count frequencies of remainders modulo 3
+        for (int stone : stones) {
+            int rem = stone % 3;
+            if (rem == 0) {
+                c0++;
+            } else if (rem == 1) {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
+        
+        if (c0 % 2 == 0) {
+            return c1 >= 1 && c2 >= 1;
+        } 
+        else {
+            return Math.abs(c1 - c2) > 2;
+        }
+    }
+}
